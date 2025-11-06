e14811f32a2d66b9d779a1626e5512eea457232c
ferd <beowulf1416@gmail.com>
2025-11-06 20:12:44 +0800

    add methods and tests

--- libs/tenants_provider/src/lib.rs
+++ libs/tenants_provider/src/lib.rs
@@ -1,20 +1,59 @@
+use tracing::{
+    info,
+    debug,
+    error
+};
+
+use uuid::Uuid;
+use core::future::Future;
+use serde::Serialize;
+use std::vec::Vec;
+
+#[derive(Debug, Serialize)]
 pub struct Tenant {
-    pub id: uuid::Uuid,
-    pub name: String
+    id: uuid::Uuid,
+    active: bool,
+    created: chrono::DateTime<chrono::Utc>,
+    name: String,
+    description: String
 }
 
 
 impl Tenant {
 
-    pub fn nil() -> Self {
+    pub fn new(
+        tenant_id: &uuid::Uuid,
+        active: bool,
+        created: &chrono::DateTime<chrono::Utc>,
+        name: &str,
+        description: &str
+    ) -> Self {
         return Self {
-            id: uuid::Uuid::nil(),
-            name: String::from("")
+            id: tenant_id.clone(),
+            active,
+            created: created.clone(),
+            name: String::from(name),
+            description: String::from(description)
         };
     }
 
-    pub fn is_nil(&self) -> bool {
-        return self.id.is_nil();
+    pub fn default() -> Self {
+        return Self {
+            id: uuid::Uuid::nil(),
+            active: true,
+            created: chrono::Utc::now(),
+            name: String::from("default"),
+            description: String::from("default")
+        };
+    }
+
+
+    pub fn tenant_id(&self) -> uuid::Uuid {
+        return self.id.clone();
+    }
+
+    pub fn name(&self) -> String {
+        return self.name.clone();
     }
 }
 
@@ -22,13 +61,37 @@ impl Tenant {
 
 pub trait TenantsProvider {
 
-    fn save(
-        &self,
-        tenant: &Tenant
-    ) -> impl Future<Output = Result<(), &'static str>> + Send;
+    // fn save(
+    //     &self,
+    //     tenant: &Tenant
+    // ) -> impl Future<Output = Result<(), &'static str>> + Send;
 
-    fn fetch_by_id(
+    // fn fetch_by_id(
+    //     &self,
+    //     tenant_id: &uuid::Uuid
+    // ) -> impl Future<Output = Result<Tenant, &'static str>> + Send;
+
+
+    fn tenants_fetch_by_id(
         &self,
         tenant_id: &uuid::Uuid
     ) -> impl Future<Output = Result<Tenant, &'static str>> + Send;
+
+    fn tenant_save(
+        &self,
+        tenant_id: &uuid::Uuid,
+        name: &str,
+        description: &str
+    ) -> impl Future<Output = Result<(), &'static str>> + Send;
+
+    fn tenant_set_active(
+        &self,
+        tenant_id: &uuid::Uuid,
+        active: bool
+    ) -> impl Future<Output = Result<(), &'static str>> + Send;
+
+    fn tenants_fetch(
+        &self,
+        filter: &str
+    ) -> impl Future<Output = Result<Vec<Tenant>, &'static str>> + Send;
 }
