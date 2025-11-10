7876a24a0eeb891341a2a1186957d320a9770bc3
ferd <beowulf1416@gmail.com>
2025-11-10 22:17:29 +0800

    add placeholder for endpoint

--- ws/src/endpoints/admin/users.rs
+++ ws/src/endpoints/admin/users.rs
@@ -3,8 +3,9 @@ use tracing::{
     debug,
     error
 };
-use std::sync::Arc;
 
+use std::sync::Arc;
+use serde::Deserialize;
 use actix_web::{
     http, 
     web, 
@@ -29,15 +30,47 @@ pub fn config(cfg: &mut web::ServiceConfig) {
                 .route(web::method(http::Method::OPTIONS).to(default_option_response))
                 .route(web::post().to(admin_users_fetch))
         )
+        .service(
+            web::resource("save")
+                .route(web::method(http::Method::OPTIONS).to(default_option_response))
+                .route(web::post().to(admin_users_save))
+        )
     ;
 }
 
 
 
-async fn admin_users_fetch(
+#[derive(Debug, Deserialize)]
+struct UsersFetchPost {
+    filter: String
+}
 
+
+async fn admin_users_fetch(
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    params: web::Json<UsersFetchPost>
 ) -> impl Responder {
     info!("admin_users_fetch");
+
+    return HttpResponse::Ok()
+        .json(ApiResponse::ok("success"))
+        ;
+}
+
+
+
+#[derive(Debug, Deserialize)]
+struct UserSavePost {
+    user_id: uuid::Uuid,
+    email: String
+}
+
+
+async fn admin_users_save(
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    params: web::Json<UserSavePost>
+) -> impl Responder {
+    info!("admin_users_save");
 
     return HttpResponse::Ok()
         .json(ApiResponse::ok("success"))
