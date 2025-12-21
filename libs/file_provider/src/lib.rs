590092776fdf022ef02eca3cdedcaa33d2479d43
ferd <beowulf1416@gmail.com>
2025-12-21 09:15:44 +0800

    add file provider

---
+++ libs/file_provider/src/lib.rs
@@ -0,0 +1,40 @@
+pub struct Folder {
+    pub folder_id: uuid::Uuid,
+    pub name: String
+}
+
+pub struct File {
+    pub file_id: uuid::Uuid,
+    pub name: String
+}
+
+
+pub trait FileProvider {
+
+    fn folder_add(
+        &self,
+        tenant_id: &uuid::Uuid,
+        folder: &Folder
+    ) -> impl Future<Output = Result<(), &'static str>> + Send;
+
+    fn folder_get(
+        &self,
+        folder_id: &uuid::Uuid
+    ) -> impl Future<Output = Result<Folder, &'static str>> + Send;
+
+    fn folder_list_files(
+        &self,
+        folder_id: &uuid::Uuid
+    ) -> impl Future<Output = Result<Vec<File>, &'static str>> + Send;
+
+    fn file_add(
+        &self,
+        tenant_id: &uuid::Uuid,
+        file: &File
+    ) -> impl Future<Output = Result<(), &'static str>> + Send;
+
+    fn file_get(
+        &self,
+        file_id: &uuid::Uuid
+    ) -> impl Future<Output = Result<File, &'static str>> + Send;
+}
---
+++ libs/file_provider_postgres/src/lib.rs
@@ -0,0 +1,167 @@
+use tracing::{
+    info,
+    error
+};
+
+pub struct PostgresFileProvider {
+    dp: database_provider::DatabaseProvider
+}
+
+
+impl PostgresFileProvider {
+    pub fn new(
+        dp: &database_provider::DatabaseProvider
+     ) -> Self {
+        return Self {
+            dp: dp.clone()
+        };
+    }
+}
+
+
+impl file_provider::FileProvider for PostgresFileProvider {
+
+    async fn folder_add(
+        &self,
+        tenant_id: &uuid::Uuid,
+        folder: &file_provider::Folder
+    ) -> Result<(), &'static str> {
+        info!("folder_add");
+
+        if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+            match sqlx::query("call files.folder_add($1,$2,$3);")
+                .bind(tenant_id)
+                .bind(folder.folder_id)
+                .bind(folder.name.clone())
+                .execute(&pool)
+                .await {
+                    Err(e) => {
+                        error!("Error adding folder record: {:?}", e);
+                        return Err("Error adding folder record");
+                    }
+                    Ok(_) => {
+                        return Ok(());
+                    }
+                }
+        }
+
+        return Err("No database pool found");
+    }
+
+    async fn folder_get(
+        &self,
+        folder_id: &uuid::Uuid
+    ) -> Result<file_provider::Folder, &'static str> {
+        info!("folder_get");
+
+        if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+            match sqlx::query("select * from fp.folder_get($1)")
+                .bind(folder_id)
+                .fetch_one(&pool)
+                .await {
+                    Err(e) => {
+                        error!("Error getting folder record: {:?}", e);
+                        return Err("Error getting folder record");
+                    }
+                    Ok(record) => {
+                        return Ok(file_provider::Folder {
+                            folder_id: record.0,
+                            name: record.1
+                        });
+                    }
+                }
+        }
+
+        return Err("No database pool found");
+    }
+
+    async fn folder_list_files(
+        &self,
+        folder_id: &uuid::Uuid
+    ) -> Result<Vec<file_provider::File>, &'static str> {
+        info!("folder_list_files");
+
+        if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+            match sqlx::query("select * from files.folder_list_files($1)")
+                .bind(folder_id)
+                .fetch_all(&pool)
+                .await {
+                    Err(e) => {
+                        error!("Error listing files in folder: {:?}", e);
+                        return Err("Error listing files in folder");
+                    }
+                    Ok(records) => {
+                        let files: Vec<file_provider::File> = records.into_iter().map(|record| {
+                            file_provider::File {
+                                file_id: record.0,
+                                name: record.1
+                            }
+                        }).collect();
+                        return Ok(files);
+                    }
+                }
+        }
+
+        return Err("No database pool found");
+    }
+
+    async fn file_add(
+        &self,
+        tenant_id: &uuid::Uuid,
+        file: &file_provider::File
+    ) -> Result<(), &'static str> {
+        info!("file_add");
+
+        if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+            match sqlx::query("call files.file_add($1,$2,$3);")
+                .bind(tenant_id)
+                .bind(file.file_id)
+                .bind(file.name.clone())
+                .execute(&pool)
+                .await {
+                    Err(e) => {
+                        error!("Error adding folder record: {:?}", e);
+                        return Err("Error adding folder record");
+                    }
+                    Ok(_) => {
+                        return Ok(());
+                    }
+                }
+        }
+
+        return Err("No database pool found");
+    }
+
+    async fn file_get(
+        &self,
+        file_id: &uuid::Uuid
+    ) -> Result<file_provider::File, &'static str> {
+        info!("file_get");
+
+        if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+            match sqlx::query("select * from files.file_get($1)")
+                .bind(file_id)
+                .fetch_one(&pool)
+                .await {
+                    Err(e) => {
+                        error!("Error getting folder record: {:?}", e);
+                        return Err("Error getting folder record");
+                    }
+                    Ok(record) => {
+                        return Ok(file_provider::File {
+                            file_id: record.0,
+                            name: record.1
+                        });
+                    }
+                }
+        }
+
+        return Err("No database pool found");
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
