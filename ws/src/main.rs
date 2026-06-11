f4e77ff32ca05d49b6a8cb25f21aa1e2be4a70fc
ferd <beowulf1416@gmail.com>
2026-06-11 21:51:52 +0800

    modify routes

--- ws/src/main.rs
+++ ws/src/main.rs
@@ -73,7 +73,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/users").configure(crate::endpoints::user::users::config))
 
             .service(web::scope("/permissions").configure(crate::endpoints::permissions::config))
-            .service(web::scope("/tenants").configure(crate::endpoints::admin::tenants::config))
+            .service(web::scope("/admin/tenants").configure(crate::endpoints::admin::tenants::config))
             .service(web::scope("/admin/users").configure(crate::endpoints::admin::users::config))
 
             // .service(web::scope("/documents").configure(crate::endpoints::documents::config))
