f5d8aac3ecaec54789ff07cac33fc9161985cf9e
ferd tomale <beowulf1416@gmail.com>
2026-07-04 15:43:45 +0800

    remove gender from partner data

--- libs/crm_provider_postgres/src/lib.rs
+++ libs/crm_provider_postgres/src/lib.rs
@@ -1,40 +1,28 @@
 #![allow(clippy::needless_return)]
 
 use tracing::{
-    info,
     error,
     // debug
+    info,
 };
 
 use sqlx::Row;
 
-
-
-
 pub struct PostgresCrmProvider {
-    dp: database_provider::DatabaseProvider
+    dp: database_provider::DatabaseProvider,
 }
 
-
 impl PostgresCrmProvider {
-
-    pub fn new(
-        dp: &database_provider::DatabaseProvider
-    ) -> Self {
-        return Self {
-            dp: dp.clone()
-        };
+    pub fn new(dp: &database_provider::DatabaseProvider) -> Self {
+        return Self { dp: dp.clone() };
     }
 }
 
-
-
 impl crm_provider::CrmProvider for PostgresCrmProvider {
-
     async fn partner_save(
         &self,
         tenant_id: &uuid::Uuid,
-        partner: &crm_provider::Partner
+        partner: &crm_provider::Partner,
     ) -> Result<(), &'static str> {
         info!("partner_save");
 
@@ -50,15 +38,16 @@ impl crm_provider::CrmProvider for PostgresCrmProvider {
                 .bind(partner.prefix.clone())
                 .bind(partner.suffix.clone())
                 .execute(&pool)
-                .await {
-                    Ok(_) => {
-                        return Ok(());
-                    }
-                    Err(e) => {
-                        error!("Error saving partner record: {:?}", e);
-                        return Err("Error saving partner record");
-                    }
+                .await
+            {
+                Ok(_) => {
+                    return Ok(());
                 }
+                Err(e) => {
+                    error!("Error saving partner record: {:?}", e);
+                    return Err("Error saving partner record");
+                }
+            }
         } else {
             error!("No Postgres pool found for 'main'");
             return Err("Unable to get pool for 'main'");
@@ -72,26 +61,26 @@ impl crm_provider::CrmProvider for PostgresCrmProvider {
     // ) -> Result<(), &'static str> {
     // 	info!("business_save");
 
-	   //  if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
-	   //      match sqlx::query("call crm.business_save($1, $2, $3, $4);")
-	   //          .bind(tenant_id)
-	   //          .bind(business.business_id.clone())
-	   //          .bind(business.name.clone())
-	   //          .bind(business.description.clone())
-	   //          .execute(&pool)
-	   //          .await {
-	   //              Ok(_) => {
-	   //                  return Ok(());
-	   //              }
-	   //              Err(e) => {
-	   //                  error!("Error saving business record: {:?}", e);
-	   //                  return Err("Error saving business record");
-	   //              }
-	   //          }
-	   //  } else {
-	   //      error!("No Postgres pool found for 'main'");
-	   //      return Err("Unable to get pool for 'main'");
-	   //  }
+    //  if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
+    //      match sqlx::query("call crm.business_save($1, $2, $3, $4);")
+    //          .bind(tenant_id)
+    //          .bind(business.business_id.clone())
+    //          .bind(business.name.clone())
+    //          .bind(business.description.clone())
+    //          .execute(&pool)
+    //          .await {
+    //              Ok(_) => {
+    //                  return Ok(());
+    //              }
+    //              Err(e) => {
+    //                  error!("Error saving business record: {:?}", e);
+    //                  return Err("Error saving business record");
+    //              }
+    //          }
+    //  } else {
+    //      error!("No Postgres pool found for 'main'");
+    //      return Err("Unable to get pool for 'main'");
+    //  }
     // }
 
     async fn partners_fetch(
@@ -99,19 +88,22 @@ impl crm_provider::CrmProvider for PostgresCrmProvider {
         tenant_id: &uuid::Uuid,
         filter: &str,
     ) -> Result<Vec<crm_provider::Partner>, &'static str> {
-   		info!("partners_fetch");
+        info!("partners_fetch");
 
         if let Some(database_provider::DatabaseType::Postgres(pool)) = self.dp.get_pool("main") {
             match sqlx::query("select * from crm.partners_fetch($1, $2);")
                 .bind(tenant_id)
                 .bind(filter)
                 .fetch_all(&pool)
-                .await {
-                    Ok(rows) => {
-                        let partners: Vec<crm_provider::Partner> = rows.iter().map(|r| {
+                .await
+            {
+                Ok(rows) => {
+                    let partners: Vec<crm_provider::Partner> = rows
+                        .iter()
+                        .map(|r| {
                             let partner_id: uuid::Uuid = r.get("partner_id");
                             let active: bool = r.get("active");
-                            let created: chrono::DateTime<chrono::Utc> = r.get("created_at");
+                            let created: chrono::DateTime<chrono::Utc> = r.get("created_ts");
                             let business_name: String = r.get("business_name");
                             let description: String = r.get("description");
                             let first_name: String = r.get("first_name");
@@ -132,16 +124,16 @@ impl crm_provider::CrmProvider for PostgresCrmProvider {
                                 prefix,
                                 suffix,
                             };
+                        })
+                        .collect();
 
-                        }).collect();
-
-                        return Ok(partners);
-                    }
-                    Err(e) => {
-                        error!("Error fetching partners: {:?}", e);
-                        return Err("Error fetching partners");
-                    }
+                    return Ok(partners);
                 }
+                Err(e) => {
+                    error!("Error fetching partners: {:?}", e);
+                    return Err("Error fetching partners");
+                }
+            }
         } else {
             error!("No Postgres pool found for 'main'");
             return Err("Unable to get pool for 'main'");
@@ -149,80 +141,79 @@ impl crm_provider::CrmProvider for PostgresCrmProvider {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     // use rand::{ distr::Alphanumeric, Rng };
 
-    use tenants_provider::TenantsProvider;
     use crm_provider::CrmProvider;
+    use tenants_provider::TenantsProvider;
 
-	#[actix_web::test]
-	async fn test_person_save() {
-	    if let Err(e) = tracing_subscriber::fmt::try_init() {
-	        println!("error: {:?}", e);
-	    }
+    #[actix_web::test]
+    async fn test_person_save() {
+        if let Err(e) = tracing_subscriber::fmt::try_init() {
+            println!("error: {:?}", e);
+        }
 
-	    let cfg = config::Config::from_env();
-	    let db_provider = database_provider::DatabaseProvider::new(&cfg);
-	    let dp = actix_web::web::Data::new(std::sync::Arc::new(db_provider));
+        let cfg = config::Config::from_env();
+        let db_provider = database_provider::DatabaseProvider::new(&cfg);
+        let dp = actix_web::web::Data::new(std::sync::Arc::new(db_provider));
 
-		let tp = tenants_provider_postgres::PostgresTenantsProvider::new(&dp);
-		let cp = PostgresCrmProvider::new(&dp);
+        let tp = tenants_provider_postgres::PostgresTenantsProvider::new(&dp);
+        let cp = PostgresCrmProvider::new(&dp);
 
-		let partner_id = uuid::Uuid::new_v4();
+        let partner_id = uuid::Uuid::new_v4();
 
-		let tenant = tp.tenant_fetch_by_name("tenant_01").await.unwrap();
-		let tenant_id = tenant.tenant_id();
+        let tenant = tp.tenant_fetch_by_name("tenant_01").await.unwrap();
+        let tenant_id = tenant.tenant_id();
 
-		let partner = crm_provider::Partner {
-			partner_id: partner_id,
-			active: true,
-			created: chrono::Utc::now(),
-			business_name: String::from(format!("test_name_{}", rand::random::<u16>())),
-			description: String::from("test_description"),
-			first_name: String::from("test_first"),
-			middle_name: String::from("test_middle"),
-			last_name: String::from("test_last"),
-			prefix: String::from("prefix"),
-			suffix: String::from("suffix"),
-			// gender: 0,
-		};
+        let partner = crm_provider::Partner {
+            partner_id: partner_id,
+            active: true,
+            created: chrono::Utc::now(),
+            business_name: String::from(format!("test_name_{}", rand::random::<u16>())),
+            description: String::from("test_description"),
+            first_name: String::from("test_first"),
+            middle_name: String::from("test_middle"),
+            last_name: String::from("test_last"),
+            prefix: String::from("prefix"),
+            suffix: String::from("suffix"),
+            // gender: 0,
+        };
 
-		if let Err(e) = cp.partner_save(&tenant_id, &partner).await {
-			error!("Error saving partner record: {:?}", e);
-			assert!(false, "Failed to save partner record");
-		}
-	}
+        if let Err(e) = cp.partner_save(&tenant_id, &partner).await {
+            error!("Error saving partner record: {:?}", e);
+            assert!(false, "Failed to save partner record");
+        }
+    }
 
-	// #[actix_web::test]
-	// async fn test_business_save() {
-	//     if let Err(e) = tracing_subscriber::fmt::try_init() {
-	//         println!("error: {:?}", e);
-	//     }
+    // #[actix_web::test]
+    // async fn test_business_save() {
+    //     if let Err(e) = tracing_subscriber::fmt::try_init() {
+    //         println!("error: {:?}", e);
+    //     }
 
-	//     let cfg = config::Config::from_env();
-	//     let db_provider = database_provider::DatabaseProvider::new(&cfg);
-	//     let dp = actix_web::web::Data::new(std::sync::Arc::new(db_provider));
+    //     let cfg = config::Config::from_env();
+    //     let db_provider = database_provider::DatabaseProvider::new(&cfg);
+    //     let dp = actix_web::web::Data::new(std::sync::Arc::new(db_provider));
 
-	// 	let tp = tenants_provider_postgres::PostgresTenantsProvider::new(&dp);
-	// 	let cp = PostgresCrmProvider::new(&dp);
+    // 	let tp = tenants_provider_postgres::PostgresTenantsProvider::new(&dp);
+    // 	let cp = PostgresCrmProvider::new(&dp);
 
-	// 	let business_id = uuid::Uuid::new_v4();
+    // 	let business_id = uuid::Uuid::new_v4();
 
-	// 	let tenant = tp.tenant_fetch_by_name("tenant_01").await.unwrap();
-	// 	let tenant_id = tenant.tenant_id();
+    // 	let tenant = tp.tenant_fetch_by_name("tenant_01").await.unwrap();
+    // 	let tenant_id = tenant.tenant_id();
 
-	// 	let business = crm_provider::Business {
-	// 		business_id: business_id,
-	// 		name: String::from(format!("test_name_{}", rand::random::<u16>())),
-	// 		description: String::from("test_description")
-	// 	};
+    // 	let business = crm_provider::Business {
+    // 		business_id: business_id,
+    // 		name: String::from(format!("test_name_{}", rand::random::<u16>())),
+    // 		description: String::from("test_description")
+    // 	};
 
-	// 	if let Err(e) = cp.business_save(&tenant_id, &business).await {
-	// 		error!("Error saving business record: {:?}", e);
-	// 		assert!(false, "Failed to save business record");
-	// 	}
-	// }
+    // 	if let Err(e) = cp.business_save(&tenant_id, &business).await {
+    // 		error!("Error saving business record: {:?}", e);
+    // 		assert!(false, "Failed to save business record");
+    // 	}
+    // }
 }
--- ws/src/endpoints/crm.rs
+++ ws/src/endpoints/crm.rs
@@ -1,24 +1,11 @@
-use tracing::{
-    info,
-    error,
-    debug
-};
+use tracing::{debug, error, info};
 
-use std::sync::Arc;
-use serde::{
-    Serialize,
-    Deserialize
-};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::sync::Arc;
 
 // use http::header::AUTHORIZATION;
-use actix_web::{
-    guard,
-    http,
-    web,
-    HttpResponse,
-    Responder
-};
+use actix_web::{HttpResponse, Responder, guard, http, web};
 
 use crate::{
     classes::{
@@ -26,96 +13,90 @@ use crate::{
         // tenant,
         // permission
     },
-    endpoints::{
-        ApiResponse,
-        default_option_response
-    }
+    endpoints::{ApiResponse, default_option_response},
 };
 
 use crm_provider::CrmProvider;
 
-
-
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg
-        .service(
-            web::resource("partner/save")
-                .route(web::method(http::Method::OPTIONS).to(default_option_response))
-                .route(web::post().guard(guard::Header("content-type", "application/json")).to(partner_save_post))
-        )
-        // .service(
-        //     web::resource("business/save")
-        //         .route(web::method(http::Method::OPTIONS).to(default_option_response))
-        //         .route(web::post().guard(guard::Header("content-type", "application/json")).to(business_save_post))
-        // )
-        .service(
-            web::resource("partners/fetch")
-                .route(web::method(http::Method::OPTIONS).to(default_option_response))
-                .route(web::post().guard(guard::Header("content-type", "application/json")).to(partners_fetch_post))
-        )
-    ;
+    cfg.service(
+        web::resource("partner/save")
+            .route(web::method(http::Method::OPTIONS).to(default_option_response))
+            .route(
+                web::post()
+                    .guard(guard::Header("content-type", "application/json"))
+                    .to(partner_save_post),
+            ),
+    )
+    // .service(
+    //     web::resource("business/save")
+    //         .route(web::method(http::Method::OPTIONS).to(default_option_response))
+    //         .route(web::post().guard(guard::Header("content-type", "application/json")).to(business_save_post))
+    // )
+    .service(
+        web::resource("partners/fetch")
+            .route(web::method(http::Method::OPTIONS).to(default_option_response))
+            .route(
+                web::post()
+                    .guard(guard::Header("content-type", "application/json"))
+                    .to(partners_fetch_post),
+            ),
+    );
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct PartnerSavePostData {
-	tenant_id: uuid::Uuid,
-	partner_id: uuid::Uuid,
+    tenant_id: uuid::Uuid,
+    partner_id: uuid::Uuid,
 
-	business_name: String,
-	description: String,
+    business_name: String,
+    description: String,
 
-	first_name: String,
+    first_name: String,
     middle_name: String,
     last_name: String,
     prefix: String,
     suffix: String,
-    gender: i16,
+    // gender: i16,
 }
 
 async fn partner_save_post(
-	dp: web::Data<Arc<database_provider::DatabaseProvider>>,
-	user: user::User,
-	params: web::Json<PartnerSavePostData>,
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    user: user::User,
+    params: web::Json<PartnerSavePostData>,
 ) -> impl Responder {
-	info!("partner_save_post");
+    info!("partner_save_post");
 
-	let crm_provider = crm_provider_postgres::PostgresCrmProvider::new(&dp);
+    let crm_provider = crm_provider_postgres::PostgresCrmProvider::new(&dp);
 
-	let partner = crm_provider::Partner {
-		partner_id: params.partner_id.clone(),
+    let partner = crm_provider::Partner {
+        partner_id: params.partner_id.clone(),
 
-		active: true,
-		created: chrono::Utc::now(),
+        active: true,
+        created: chrono::Utc::now(),
 
-		business_name: params.business_name.clone(),
-		description: params.description.clone(),
+        business_name: params.business_name.clone(),
+        description: params.description.clone(),
 
-		first_name: params.first_name.clone(),
-		middle_name: params.middle_name.clone(),
-		last_name: params.last_name.clone(),
-		prefix: params.prefix.clone(),
-		suffix: params.suffix.clone(),
-		// gender: params.gender.clone(),
-	};
+        first_name: params.first_name.clone(),
+        middle_name: params.middle_name.clone(),
+        last_name: params.last_name.clone(),
+        prefix: params.prefix.clone(),
+        suffix: params.suffix.clone(),
+        // gender: params.gender.clone(),
+    };
 
-	match crm_provider.partner_save(
-		&params.tenant_id,
-		&partner,
-	).await {
-		Err(e) => {
-			error!("unable to save partner record: {}", e);
+    match crm_provider.partner_save(&params.tenant_id, &partner).await {
+        Err(e) => {
+            error!("unable to save partner record: {}", e);
             return HttpResponse::InternalServerError()
                 .json(ApiResponse::error("unable to save partner record"));
-		}
-		Ok(_) => {
-			return HttpResponse::Ok().json(
-				ApiResponse::ok("successfully saved partner record")
-			);
-		}
-	}
+        }
+        Ok(_) => {
+            return HttpResponse::Ok().json(ApiResponse::ok("successfully saved partner record"));
+        }
+    }
 }
-
 
 // #[derive(Debug, Serialize, Deserialize)]
 // struct BusinessSavePostData {
@@ -157,43 +138,40 @@ async fn partner_save_post(
 // 	}
 // }
 
-
 #[derive(Debug, Serialize, Deserialize)]
 struct PartnersFetchPostData {
-	tenant_id: uuid::Uuid,
-	filter: String
+    tenant_id: uuid::Uuid,
+    filter: String,
 }
 
 async fn partners_fetch_post(
-	dp: web::Data<Arc<database_provider::DatabaseProvider>>,
-	user: user::User,
-	params: web::Json<PartnersFetchPostData>,
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    user: user::User,
+    params: web::Json<PartnersFetchPostData>,
 ) -> impl Responder {
-	info!("partners_fetch_post");
+    info!("partners_fetch_post");
 
-	let crm_provider = crm_provider_postgres::PostgresCrmProvider::new(&dp);
+    let crm_provider = crm_provider_postgres::PostgresCrmProvider::new(&dp);
 
     let filter = format!("%{}%", params.filter);
 
-    match crm_provider.partners_fetch(
-        &params.tenant_id,
-        &filter
-    ).await {
+    match crm_provider
+        .partners_fetch(&params.tenant_id, &filter)
+        .await
+    {
         Err(e) => {
             error!("unable to fetch permissions: {}", e);
             return HttpResponse::InternalServerError()
                 .json(ApiResponse::error("unable to fetch permissions"));
         }
         Ok(partners) => {
-            return HttpResponse::Ok()
-                .json(ApiResponse::new(
-                    true,
-                    "successfully fetched permissions",
-                    Some(json!({
-                        "partners": partners
-                    })
-                )));
+            return HttpResponse::Ok().json(ApiResponse::new(
+                true,
+                "successfully fetched permissions",
+                Some(json!({
+                    "partners": partners
+                })),
+            ));
         }
     }
-
 }
