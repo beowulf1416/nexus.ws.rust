69b4b3d8a196264e1d825c2c50722cde6b8d0285
ferd tomale <beowulf1416@gmail.com>
2026-07-22 22:16:24 +0800

    add endpoint to fetch tree of organizations

--- ws/src/endpoints/organizations.rs
+++ ws/src/endpoints/organizations.rs
@@ -1,3 +1,4 @@
+use actix_http::Response;
 use tracing::{debug, error, info};
 
 use std::sync::Arc;
@@ -19,6 +20,16 @@ pub fn config(cfg: &mut web::ServiceConfig) {
         web::resource("fetch")
             .route(web::method(http::Method::OPTIONS).to(default_option_response))
             .route(web::post().to(organizations_fetch_post)),
+    )
+    .service(
+        web::resource("fetch/tree")
+            .route(web::method(http::Method::OPTIONS).to(default_option_response))
+            .route(web::post().to(organizations_fetch_tree_post)),
+    )
+    .service(
+        web::resource("save")
+            .route(web::method(http::Method::OPTIONS).to(default_option_response))
+            .route(web::post().to(organization_save_post)),
     );
 }
 
@@ -52,6 +63,80 @@ async fn organizations_fetch_post(
                 Some(json!({
                     "organizations": organizations
                 })),
+            ));
+        }
+    }
+}
+
+async fn organizations_fetch_tree_post(
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    user: user::User,
+) -> impl Responder {
+    info!("organizations_fetch_tree_post");
+
+    let opp = tenants_provider_postgres::organizations::OrganizationsProviderPostgres::new(&dp);
+
+    let tenant_id = user.tenant().tenant_id();
+
+    match opp.fetch_tree(&tenant_id).await {
+        Err(e) => {
+            error!("unable to fetch organizations: {}", e);
+            return HttpResponse::InternalServerError()
+                .json(ApiResponse::error("unable to fetch organizations"));
+        }
+        Ok(organizations) => {
+            return HttpResponse::Ok().json(ApiResponse::new(
+                true,
+                "successfully fetched organizations",
+                Some(json!({
+                    "organizations": organizations
+                })),
+            ));
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct OrganizationSavePost {
+    org_id: uuid::Uuid,
+    parent_org_id: uuid::Uuid,
+    name: String,
+    description: String,
+    version: i32,
+}
+
+async fn organization_save_post(
+    dp: web::Data<Arc<database_provider::DatabaseProvider>>,
+    user: user::User,
+    params: web::Json<OrganizationSavePost>,
+) -> impl Responder {
+    info!("organization_save_post");
+
+    let opp = tenants_provider_postgres::organizations::OrganizationsProviderPostgres::new(&dp);
+
+    let tenant_id = user.tenant().tenant_id();
+
+    match opp
+        .save(
+            &tenant_id,
+            &params.org_id,
+            &params.parent_org_id,
+            &params.name,
+            &params.description,
+            &params.version,
+        )
+        .await
+    {
+        Err(e) => {
+            error!("unable to save organization: {}", e);
+            return HttpResponse::InternalServerError()
+                .json(ApiResponse::error("unable to save organization"));
+        }
+        Ok(_) => {
+            return HttpResponse::Ok().json(ApiResponse::new(
+                true,
+                "successfully fetched organizations",
+                None,
             ));
         }
     }
